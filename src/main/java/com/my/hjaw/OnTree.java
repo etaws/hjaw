532259a8cd8e9566bfdf6093d0c73d9a9525ff8d
etaws <etawsz@gmail.com>
2022-07-19 18:27:43 +0800

    pass pre

--- src/main/java/com/my/hjaw/OnTree.java
+++ src/main/java/com/my/hjaw/OnTree.java
@@ -38,4 +38,37 @@ public class OnTree {
 
         return ret.toArray(new Integer[0]);
     }
+
+    public Integer[] pre(Integer[] vals) {
+
+        TreeNode root = TreeNode.createTree(vals);
+
+        List<TreeNode> stack = new LinkedList<>();
+
+        ArrayList<Integer> ret = new ArrayList<>();
+
+        TreeNode node = root;
+
+        while (true) {
+            while (node != null) {
+
+                ret.add(node.val);
+
+                stack.add(node);
+                node = node.left;
+            }
+
+            if (stack.isEmpty()) {
+                break;
+            }
+
+            node = stack.get(stack.size() - 1);
+            stack.remove(stack.size() - 1);
+
+
+            node = node.right;
+        }
+
+        return ret.toArray(new Integer[0]);
+    }
 }
--- src/test/java/com/my/hjaw/OnTreeTest.java
+++ src/test/java/com/my/hjaw/OnTreeTest.java
@@ -19,4 +19,19 @@ public class OnTreeTest {
             System.out.printf(" " + show);
         }
     }
+
+    @Test
+    public void testPre() {
+        Integer[] vals = new Integer[]{7, 9, 8, 5, 10, null, 12, 6, null, 13, null};
+        Integer[] expects = new Integer[]{7, 9, 5, 6, 10, 13, 8, 12};
+
+        Integer[] shows = new OnTree().pre(vals);
+
+        assertArrayEquals(shows, expects);
+
+        System.out.println("tree size: " + shows.length);
+        for (Integer show : shows) {
+            System.out.printf(" " + show);
+        }
+    }
 }
