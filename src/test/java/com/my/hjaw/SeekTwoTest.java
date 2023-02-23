f532665780ea152675f05fda6871fa49cd9c396a
etaws <etawsz@gmail.com>
2023-02-23 12:25:34 +0800

    add more test cases for seek_two

--- src/test/java/com/my/hjaw/SeekTwoTest.java
+++ src/test/java/com/my/hjaw/SeekTwoTest.java
@@ -10,6 +10,10 @@ public class SeekTwoTest {
 
         checkSeekTwo(new int[]{3, 9, 20, 15, 7}, 19, new int[]{0, 0});
         checkSeekTwo(new int[]{3, 9, 20, 15, 7}, 18, new int[]{0, 3});
+        checkSeekTwo(new int[]{15, 11, 7, 2}, 9, new int[]{2, 3});
+        checkSeekTwo(new int[]{2, 3, 4}, 6, new int[]{0, 2});
+        checkSeekTwo(new int[]{3, 3}, 6, new int[]{0, 1});
+
     }
 
     private void checkSeekTwo(int[] vals, int target, int[] results) {
