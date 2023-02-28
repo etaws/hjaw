58fdbd8d2f2d435dfc27820b20aeaec381a6e230
etaws <etawsz@gmail.com>
2023-02-28 19:20:46 +0800

    pass more unit tests

--- src/test/java/com/my/hjaw/Expand.java
+++ src/test/java/com/my/hjaw/Expand.java
@@ -7,133 +7,76 @@ public class Expand {
 
     public String expand(String s) {
 
-        StringBuilder r = new StringBuilder();
-
-        // 用状态机来实现。2 个状态：
-        // 0：默认状态，非栈内
-        // 1：栈内
-        int state = 0;
-
-        List<Integer> nStack = new LinkedList<>();
-        List<String> sStack = new LinkedList<>();
+        List<Character> sStack = new LinkedList<>();
 
         int i = 0;
         while (i < s.length()) {
-            assert nStack.size() == sStack.size();
-
             char c = s.charAt(i);
 
-            if (state == 0 && isNum(c)) {
-                // 默认状态时，遇到一个「数字」
-                String num = getNumber(i, s);
-                int n = Integer.parseInt(num);
-                if (n <= 0) {
-                    throw new RuntimeException("bad str pattern: number should be positive");
-                }
-                nStack.add(n);
-                // 初始化「当前栈」
-                sStack.add("");
-                i = i + num.length() + 1;
-
-                // 进入状态 1：栈内操作
-                state = 1;
-            } else if (state == 0 && !isNum(c)) {
-                // 默认状态时，遇到一个「非数字」
-                r.append(c);
-                i++;
-            } else if (state == 1 && !isNum(c)) {
-                if (c == ']') {
-                    // 栈内时，是 ]，出栈
-                    if (nStack.isEmpty()) {
-                        // 但如果是栈是空的，直接输出当前字符就行了
-                        r.append(c);
-                    } else {
-                        // 获取栈的数据
-                        String in = getLast(sStack);
-                        int n = getLast(nStack);
-                        // 扩展
-                        StringBuilder out = new StringBuilder();
-                        for (int j = 0; j < n; j++) {
-                            out.append(in);
-                        }
-                        // 弹出堆栈
-                        removeLast(sStack);
-                        removeLast(nStack);
-                        // 输出数据（直接输出，或者放入上一层堆栈的数据中）
-                        if (sStack.isEmpty()) {
-                            r.append(out);
-                            // 结束栈内操作
-                            state = 0;
-                        } else {
-                            updateStackTop(sStack, out.toString());
-                        }
-                    }
-                } else {
-                    // 栈内时，非数字，且不是 ]，就给「当前栈」收集字符
-                    updateStackTop(sStack, "" + c);
-                }
-                i++;
-            } else if (state == 1 && isNum(c)) {
-                // 栈内时遇到数字的话，需要新增一个栈
-
-                // 解析出数字，并压栈
-                String num = getNumber(i, s);
-                int n = Integer.parseInt(num);
-                if (n <= 0) {
-                    throw new RuntimeException("bad str pattern: number should be positive");
-                }
-                nStack.add(n);
-                sStack.add("");
-                i = i + num.length() + 1;
+            if (c != ']') {
+                sStack.add(c);
             } else {
-                assert false;
+                Character top = removeLast(sStack);
+
+                List<Character> inner = new LinkedList<>();
+                while (top != '[') {
+                    inner.add(top);
+                    top = removeLast(sStack);
+                }
+
+                if (sStack.isEmpty()) {
+                    i += 1;
+                    continue;
+                }
+
+                top = removeLast(sStack);
+                int n = 0;
+                int d = 1;
+                while (true) {
+                    n += ((top - '0') * d);
+                    d *= 10;
+
+                    if (sStack.isEmpty() || !isNum(getLast(sStack))) {
+                        break;
+                    }
+
+                    top = removeLast(sStack);
+                }
+
+                int j = 0;
+                while (j < n) {
+                    int k = inner.size();
+                    while (k > 0) {
+                        sStack.add(inner.get(k - 1));
+                        k--;
+                    }
+
+                    j++;
+                }
             }
+
+            ++i;
         }
+
+        int ii = 0;
+        StringBuilder r = new StringBuilder();
+        while (ii < sStack.size()) {
+            if (isNum(sStack.get(ii))) {
+                return "";
+            }
+            r.append(sStack.get(ii));
+            ii++;
+        }
+
         return r.toString();
     }
 
-    private <T> void removeLast(List<T> list) {
-        list.remove(list.size() - 1);
+    private <T> T removeLast(List<T> list) {
+        return list.remove(list.size() - 1);
     }
 
     private <T> T getLast(List<T> list) {
         return list.get(list.size() - 1);
-    }
-
-    // 更新 List 的最后一个 string：last = last + s
-    private void updateStackTop(List<String> sStack, String s) {
-        String last = sStack.get(sStack.size() - 1);
-        last = last + s;
-        sStack.set(sStack.size() - 1, last);
-    }
-
-    // e.g.
-    // s is 'aa34[ab]'
-    // i is 2
-    // the the return is 34
-    private String getNumber(int i, String s) {
-
-        if (i + 2 > s.length()) {
-            throw new RuntimeException("bad str pattern: missing []");
-        }
-
-        StringBuilder r = new StringBuilder();
-        while (i < s.length()) {
-
-            char c = s.charAt(i);
-            if ((c != '[') && !isNum(c)) {
-                throw new RuntimeException("bad str pattern: missing [");
-            }
-
-            if (c == '[') {
-                return r.toString();
-            }
-
-            r.append(c);
-            i++;
-        }
-
-        throw new RuntimeException("bad str pattern: missing [");
     }
 
     private boolean isNum(char c) {
--- src/test/java/com/my/hjaw/ExpandTest.java
+++ src/test/java/com/my/hjaw/ExpandTest.java
@@ -7,9 +7,13 @@ public class ExpandTest {
 
     @Test
     public void checkIt() {
-        Assert.assertEquals("abcdefg", new Expand().expand("abcdefg"));
-        Assert.assertEquals("aaabcbcd", new Expand().expand("3[a]2[bc]d"));
-        Assert.assertEquals("accaccaccd", new Expand().expand("3[a2[c]]d"));
+        Assert.assertEquals("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef", new Expand().expand("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"));
+        Assert.assertEquals("abababababababababab", new Expand().expand("10[ab]"));
+        Assert.assertEquals("aaabcbc", new Expand().expand("3[a]2[bc]"));
+        Assert.assertEquals("accaccacc", new Expand().expand("3[a2[c]]"));
+        Assert.assertEquals("abcabccdcdcdef", new Expand().expand("2[abc]3[cd]ef"));
+        Assert.assertEquals("abccdcdcdxyz", new Expand().expand("abc3[cd]xyz"));
+        Assert.assertEquals("", new Expand().expand("3"));
     }
 
 }
