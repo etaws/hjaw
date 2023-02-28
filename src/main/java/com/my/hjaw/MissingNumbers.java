07b388795f6e5433c124676fc3e2d3273beed193
etaws <etawsz@gmail.com>
2023-02-28 20:17:16 +0800

    better version

--- src/main/java/com/my/hjaw/MissingNumbers.java
+++ src/main/java/com/my/hjaw/MissingNumbers.java
@@ -5,34 +5,36 @@ import java.util.List;
 
 public class MissingNumbers {
 
-    public Integer[] getMissing(Integer[] a) {
+    public Integer[] getMissing(Integer[] nums) {
 
-        for (Integer v : a) {
-            flopIt(v, a);
+        int i = 0;
+        while (i < nums.length) {
+            // 第几个数已经存在？如果是第 1 个数存在，那么 j 就是 0
+
+            int j = 0;
+            if (nums[i] < 0) {
+                j = -nums[i] - 1;
+            } else {
+                j = nums[i] - 1;
+            }
+
+
+            // 如果 j 这个数已经存在，那么把 a[j] 改为负数，作为 j 存在的标志
+            if (nums[j] > 0) {
+                nums[j] = -nums[j];
+            }
+
+            i += 1;
         }
 
         List<Integer> r = new ArrayList<>();
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > 0) {
-                r.add(i + 1);
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] > 0) {
+                r.add(j + 1);
             }
         }
+
         return r.toArray(r.toArray(new Integer[0]));
-    }
-
-    private void flopIt(int v, Integer[] a) {
-        if (v < 0) {
-            v = -v;
-        }
-
-        assert (v >= 1) && (v <= a.length);
-
-        v = v - 1;
-        if (a[v] <= 0) {
-            return;
-        }
-
-        a[v] = -a[v];
     }
 }
